271e2b2dd2012c26f682b3e71d5677de1ee2f8f0
HungWei-Andy <r05942039@ntu.edu.tw>
2017-04-08 17:01:09 +0800

    New Model uploaded

--- nlu/download.sh
+++ nlu/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget https://www.dropbox.com/s/esv9jhz8dh67ikw/intent_rnn.h5
-wget https://www.dropbox.com/s/8ivyulw0zm1i1fk/label_rnn.h5
+wget https://www.dropbox.com/s/4nztn154r9teqkm/intent_rnn.h5
+wget https://www.dropbox.com/s/uj9ugeowi2wvwqt/label_rnn.h5
 mkdir models
 mv intent_rnn.h5 models
 mv label_rnn.h5 models
